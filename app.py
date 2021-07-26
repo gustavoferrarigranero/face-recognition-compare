899a0504c25ef2773ef72d7d0d3474ba7c700415
Gustavo <gustavoferrarigranero@gmail.com>
2021-07-26 11:55:24 -0300

    ports

--- app.py
+++ app.py
@@ -13,3 +13,7 @@ def faces():
       request.files.get('image'), request.files.get('confirm')
     )
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 80))
+    app.run(host='0.0.0.0', port=port)
+
